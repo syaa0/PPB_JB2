13271b69c623a62e2371e515160be00c29c977fe
syaa0 <meisya21102022@gmail.com>
2023-12-03 13:31:22 +0700

    PPB JB1

---
+++ app/src/main/java/programmer/jobsheet1/MainActivity.kt
@@ -0,0 +1,40 @@
+package programmer.jobsheet1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var buttonButton: Button
+    private lateinit var buttonTextView: TextView
+
+    private fun initComponents(){
+        nameEditText = findViewById(R.id.nameEditText)
+        buttonButton = findViewById(R.id.buttonButton)
+        buttonTextView = findViewById(R.id.ButtonTextView)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hello_word)
+
+        initComponents()
+
+
+        buttonTextView.text = resources.getString(R.string.app_name)
+
+        buttonButton.setOnClickListener{
+            val name = nameEditText.text.toString()
+            buttonTextView.text = resources.getString(R.string.buttonTextView,name)
+
+            resources.getStringArray(R.array.names).forEach {
+                Log.i("PZN",it)
+            }
+        }
+    }
+}
